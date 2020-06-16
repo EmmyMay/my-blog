f946be5c6a62d4f6ab68664adf744b1240accdef
EmmyMay <lekeemmy@gmail.com>
2020-06-16 01:31:12 +0100

    chhange in config:gridsome

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,34 +6,7 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
-
   plugins: [{
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'posts/**/*.md',
-        typeName: 'Post',
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`
-      }
-    },
-  ]
+    use: `gridsome-plugin-netlify-cms`
+  }],
 }
